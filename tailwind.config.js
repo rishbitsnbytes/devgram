2adab82758dbc6f8edd6da70d302b2adc521ca4c
rishbitsbytes <rishrathore1613@gmail.com>
2022-09-28 01:08:07 +0530

    initialisation

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  darkMode: "class",
+  theme: {
+    extend: {},
+    screens: {
+      sm: "640px",
+      // => @media (min-width: 640px) { ... }
+
+      md: "768px",
+      // => @media (min-width: 768px) { ... }
+
+      lg: "992px",
+      // => @media (min-width: 992px) { ... }
+
+      xl: "1024px",
+      // => @media (min-width: 1024px) { ... }
+
+      "2xl": "1280px",
+      // => @media (min-width: 1280px) { ... }
+
+      "3xl": "1536px",
+      // => @media (min-width: 1536px) { ... }
+    },
+  },
+
+  plugins: [],
+};
