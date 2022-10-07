464d78b038b9d957f642f86ab9abc34636fadb14
rishbitsbytes <rishrathore1613@gmail.com>
2022-10-07 15:17:42 +0530

    update

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -7,17 +7,17 @@ const Footer = () => {
         <p className="text-center flex flex-row items-center justify-center author gap-1.5 flex-wrap text-lg">
           <i className="fa-solid fa-code"></i> by
           <a
-            href="https://bharati-21.github.io/"
+            href="https://rishbitsnbytes.netlify.app/"
             target="_blank"
             className="text-sky-400 hover:scale-95 transition-all ease-in"
           >
-            Bharati Subramanian
+            Rishabh Rathore
           </a>
         </p>
         <ul className="footer-navlinks flex flex-row items-center justify-center gap-4">
           <li className="list-item hover:translate-y-[-7px] transition-all ease-in hover:text-sky-500 text-sky-400 text-xl">
             <a
-              href="https://twitter.com/_bhaaratii"
+              href="https://twitter.com/rishbitsnbytes"
               target="_blank"
               className="btn btn-link"
             >
@@ -28,7 +28,7 @@ const Footer = () => {
           </li>
           <li className="list-item hover:translate-y-[-7px] transition-all ease-in hover:text-sky-500 text-sky-400 text-xl">
             <a
-              href="https://github.com/bharati-21"
+              href="https://github.com/rishbitsnbytes"
               target="_blank"
               className="btn btn-link"
             >
@@ -39,7 +39,7 @@ const Footer = () => {
           </li>
           <li className="list-item hover:translate-y-[-7px] transition-all ease-in hover:text-sky-500 text-sky-400 text-xl">
             <a
-              href="https://www.linkedin.com/in/bharati-subramanian-29734b152/"
+              href="https://www.linkedin.com/in/rishrathore/"
               className="btn btn-link"
               target="_blank"
             >
@@ -50,7 +50,7 @@ const Footer = () => {
           </li>
           <li className="list-item hover:translate-y-[-7px] transition-all ease-in hover:text-sky-500 text-sky-400 text-xl">
             <a
-              href="mailto:bharatisharada@gmail.com"
+              href="mailto:rishrathore1613@gmail.com"
               className="btn btn-link"
               target="_blank"
             >
